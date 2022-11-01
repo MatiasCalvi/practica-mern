7549942ad519bc6f567b79fa17c45ac60998b374
MatiasCalvi <alejandropreuss30@gmail.com>
2022-11-01 01:55:55 -0300

    tarea2

---
+++ src/events1.js
@@ -0,0 +1,30 @@
+let data = [
+    {
+        category: "Cinema",
+        name: "The new mutants",
+        image: "https://storage.googleapis.com/amazing-events-3132/events/cinema2.jpg",
+        description: "Five young mutants discover their abilities while being held in a secret facility against their will, struggling to escape their past sins and save themselves."
+    },{
+        category: "Museum",
+        name: "Jurassic Park",
+        image: "https://storage.googleapis.com/amazing-events-3132/events/mus1.jpg",
+        description: "Let's go meet the biggest dinosaurs in the paleontology museum."
+    },{
+        category: "Race",
+        name: "Cancer Marathon",
+        image: "https://storage.googleapis.com/amazing-events-3132/events/maraton8.jpg",
+        description: "We'll be raising funds for the cancer patients."
+    },{
+        category: "Party",
+        name: "Spring Day",
+        image: "https://storage.googleapis.com/amazing-events-3132/events/party3.jpg",
+        description: "Come celebrate spring dressed as your favourite character."
+    },{
+        category: "Concert",
+        name: "Reggaeton 2022",
+        image: "https://storage.googleapis.com/amazing-events-3132/events/concer6.jpg",
+        description: "Get ready to dance to the rhythm of the best bands."
+    }
+]
+
+export default data
